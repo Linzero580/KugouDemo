1653a8c7ebdcac3108ed5e32c20ad1c6714a8dc8
zero <306524909@qq.com>
2017-04-30 19:42:16 +0800

    Increased WebView in Fragment

--- app/src/main/java/com/example/linzero/kugoudemo/ListenMainTabFragment.java
+++ app/src/main/java/com/example/linzero/kugoudemo/ListenMainTabFragment.java
@@ -1,17 +1,11 @@
 package com.example.linzero.kugoudemo;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.Toast;
 
 /**
  * Created by Linzero on 2017/04/20.
@@ -19,59 +13,9 @@ import android.widget.Toast;
 
 public class ListenMainTabFragment extends Fragment {
 
-    private WebView webView_listen;
-
-    private View mContentView;
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        kugouWebView();
-    }
-
-    private Handler handler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case 1:
-                    webview_listen();
-                    break;
-            }
-        }
-    };
-
-    private void webview_listen() {
-        webView_listen.goBack();
-    }
-
-
     @Nullable
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-//        return inflater.inflate(R.layout.tab01, container, false);
-        return webView_listen;
+        return inflater.inflate(R.layout.tab01, container, false);
     }
 
-    private void kugouWebView() {
-        webView_listen = new WebView(getActivity());
-        webView_listen.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
-            }
-        });
-        webView_listen.getSettings().setJavaScriptEnabled(true);    //设置webview属性，运行执行js脚本
-        webView_listen.loadUrl("http://www.runoob.com/w3cnote/android-tutorial-intro.html"); //调用loadurl方法为webview加入链接
-
-        webView_listen.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if ((keyCode == KeyEvent.KEYCODE_BACK) && webView_listen.canGoBack()) {
-                    handler.sendEmptyMessage(1);
-                    return true;
-                }
-                return false;
-            }
-        });
-    }
 }
